f082edfd9ee0580e8aa2b0d928f24e0913d23f44
Hugo Bouderlique <bouderliqueh@gmail.com>
2022-08-20 10:42:44 +1000

    Tidy up app.ts to match coding guidelines

--- source/app.ts
+++ source/app.ts
@@ -1,12 +1,12 @@
-import express = require("express")
+import express = require("express");
 
-const app = express();
+const app  = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.send("Hello World!");
+});
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+    console.log(`listening on port ${port}`);
+});
