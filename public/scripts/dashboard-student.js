e0aa01778359183d8169c30cb52bc9220f20b1a5
srujan <srujan.sharma77k@gmail.com>
2022-09-18 00:00:06 +1000

    display students group

--- public/scripts/dashboard-student.js
+++ public/scripts/dashboard-student.js
@@ -1,7 +1,7 @@
 // Get the table body element
 const group_table_body = document.getElementById("group_table_body");
-const group_table      = document.getElementById("group_table");
-const no_group_msg     = document.getElementById("no_group_msg");
+const group_table = document.getElementById("group_table");
+const no_group_msg = document.getElementById("no_group_msg");
 
 // Function is async so we can use await keyword
 async function get_group_members() {
@@ -14,19 +14,23 @@ async function get_group_members() {
 
     if (student_names.length === 0) {
         no_group_msg.style.display = "flex";
+        group_table.hidden = true;
         return;
     } else {
-        group_table.style.display = "block";
+        group_table.hidden = false;
     }
     // Loop through the names and create a row for each student
-    for(let student_name of student_names) {
+    for (let student_name of student_names) {
         let new_row = document.createElement("tr");
         let name_cell = document.createElement("td");
         let role_cell = document.createElement("td");
         name_cell.textContent = student_name;
-        role_cell.textContent = "TBD";
+        role_cell.textContent = "No Role Assigned";
+        name_cell.classList.add('text-xl', 'border-r', 'py-2');
+        role_cell.classList.add('text-xl');
         new_row.appendChild(name_cell);
         new_row.appendChild(role_cell);
+        new_row.classList.add('border', 'hover:bg-gray-700', 'hover:cursor-text', 'py-2');
         group_table_body.appendChild(new_row);
     }
 }
