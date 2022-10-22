99888b08d5b6f6f28f2e48da3ae50b4a72f12ec2
srujan <srujan.sharma77k@gmail.com>
2022-10-22 21:08:57 +1100

    a few minor changes

--- public/scripts/admin_interests.js
+++ public/scripts/admin_interests.js
@@ -5,22 +5,22 @@ const interest_table = document.getElementById("interest_table");
 async function get_interestsList() {
     let response = await fetch("/interests");
     let interest_names = await response.json();
-    for(let interest_name of interest_names) {
-        
+    for (let interest_name of interest_names) {
+
         let new_row = document.createElement("tr");
         let name_cell = document.createElement("td");
-        
+
         let add_interest = document.createElement("INPUT");
         add_interest.setAttribute("type", "checkbox");
-        
+
         name_cell.textContent = interest_name;
         name_cell.classList.add('text-xl', 'border-r', 'py-2');
-        
+
         new_row.append(name_cell);
         new_row.append(add_interest);
 
         name_cell.classList.add('text-xl', 'border-r', 'py-2');
-        new_row.classList.add('border', 'hover:bg-gray-700', 'hover:cursor-text', 'py-2');
+        new_row.classList.add('border', 'hover:bg-gray-300', 'hover:cursor-text', 'py-2');
         interest_table_body.appendChild(new_row);
     }
 }
